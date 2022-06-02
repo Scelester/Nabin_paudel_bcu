dd56026f56d7031b2fc1c2ccecf1e8881c7c9f12
nabin paudel <nabin@localhost.localdomain>
2022-06-02 17:24:36 +0545

    dsf

--- coading_challange/task_1.py
+++ coading_challange/task_1.py
@@ -19,32 +19,35 @@ def calculate(x, y, op):
     elif op == "power":
         result = x**y
 
-    print("Your result is:", result)
+    result =  "{:.2f}".format(result)
+
+    print(f"{x} {op} {y} = {result}")
 
 
-def display_operators_info(x, y):
+
+def display_operators_info():
     """displays operators option to user"""
     print(f""" 
-        '+' to add {x} and {y}
-        '-' to subtract {y} from {x}
-        '*' to multiply {x} by {y}
-        '/' to divide {x} by {y}
-        'mod' to get reminder after division of {x} by {y}
-        'power' to get product after {x} to the power {y}
+        '+' to add 
+        '-' to subtract 
+        '*' to multiply
+        '/' to divide 
+        'mod' to get reminder
+        'power' to product of power
             """)
 
 
 def main():
     print("\n"+"====="*5)
+    display_operators_info()  # function call <-
 
+    inp_op = input("\nSelect an Operator:")  # input 3
+    
     inp_x = int(input("Enter the first number:"))  # input 1
     inp_y = int(input("Enter the second number:"))  # input 2
 
     print("Enter a operator accordingly:-")
 
-    display_operators_info(inp_x, inp_y)  # function call <-
-
-    inp_op = input("\nOperator:")  # input 3
 
     calculate(inp_x, inp_y, inp_op)  # function call <-
 
